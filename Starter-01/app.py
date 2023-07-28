f553d78e481ca46f754cdd2fb029fb66792d3f12
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-07-28 15:48:31 -0500

    change folder name to static

--- Starter-01/app.py
+++ Starter-01/app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 load_dotenv()
 
-app =   Flask(__name__, static_folder='./build', static_url_path='/')
+app =   Flask(__name__, static_folder='./static', static_url_path='/')
 
 @app.route('/', methods = ['GET'])
 def index():
